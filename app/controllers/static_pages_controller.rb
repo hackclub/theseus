c260578f28f738d28226ddf26d187e3a8af866d8
24c02 <163450896+24c02@users.noreply.github.com>
2025-04-29 14:47:10 -0400

    no meow

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,6 @@ class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :login ]
   # skip_after_action :verify_authorized
   def index
-    flash[:alert] = "meow"
   end
 
   def login
