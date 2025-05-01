dbd31d8359ebe4deefdd382210d6ba9e35a18faf
24c02 <163450896+24c02@users.noreply.github.com>
2025-05-01 11:30:40 -0400

    better hcpcxc

--- app/lib/snail_mail/templates/hcpcxc_template.rb
+++ app/lib/snail_mail/templates/hcpcxc_template.rb
@@ -32,7 +32,22 @@ module SnailMail
         # Render IMb barcode
         render_imb(pdf, letter, 240, 24, 183)
 
+        render_qr_code(pdf, letter, 5, 65, 60)
+
         render_letter_id(pdf, letter, 10, 19, 10)
+        if letter.rubber_stamps.present?
+          pdf.font("arial") do
+            pdf.text_box(
+              letter.rubber_stamps,
+              at: [ 294, 220 ],
+              width: 255,
+              height: 21,
+              overflow: :shrink_to_fit,
+              disable_wrap_by_char: true,
+              min_size: 1
+            )
+          end
+        end
         render_postage(pdf, letter)
       end
     end
