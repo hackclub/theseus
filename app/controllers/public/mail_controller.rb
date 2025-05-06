14d0370cf1527f4627a9efa87d8019c5d1c0aad9
24c02 <163450896+24c02@users.noreply.github.com>
2025-05-06 17:55:51 -0400

    initial crack at lsv

--- app/controllers/public/mail_controller.rb
+++ app/controllers/public/mail_controller.rb
@@ -6,6 +6,10 @@ module Public
       @mail =
         Warehouse::Order.where(recipient_email: current_public_user.email) +
           Letter.where(recipient_email: current_public_user.email)
+      unless params[:no_load_lsv]
+        @mail += LSV::TYPES.map { |type| type.find_by_email(current_public_user.email) }.flatten
+      end
+      @mail.sort_by!(&:created_at).reverse!
     end
   end
 end
