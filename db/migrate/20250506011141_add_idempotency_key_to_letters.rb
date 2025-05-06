d03b1e82ea90f7a0971fbfd3e6b56ca66a1b6d62
24c02 <163450896+24c02@users.noreply.github.com>
2025-05-05 22:11:37 -0400

    fix migrations?

--- db/migrate/20250506011141_add_idempotency_key_to_letters.rb
+++ db/migrate/20250506011141_add_idempotency_key_to_letters.rb
@@ -1,6 +1,6 @@
 class AddIdempotencyKeyToLetters < ActiveRecord::Migration[8.0]
   def change
-    add_column :letters, :idempotency_key, :string
-    add_index :letters, :idempotency_key, unique: true
+    add_column :letters, :idempotency_key, :string, if_not_exists: true
+    add_index :letters, :idempotency_key, unique: true, if_not_exists: true
   end
 end
