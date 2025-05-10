b23c4ee6e28f2c3a14d9a6b5619816f1320d30d5
24c02 <163450896+24c02@users.noreply.github.com>
2025-05-09 21:23:18 -0400

    if not exists

--- db/migrate/20240314000000_add_instant_queue_attributes_to_letter_queues.rb
+++ db/migrate/20240314000000_add_instant_queue_attributes_to_letter_queues.rb
@@ -1,13 +1,13 @@
 class AddInstantQueueAttributesToLetterQueues < ActiveRecord::Migration[8.0]
   def change
-    add_column :letter_queues, :type, :string
-    add_column :letter_queues, :template, :string
-    add_column :letter_queues, :postage_type, :string
-    add_column :letter_queues, :usps_payment_account_id, :bigint
-    add_column :letter_queues, :include_qr_code, :boolean, default: true
-    add_column :letter_queues, :letter_mailing_date, :date
+    add_column :letter_queues, :type, :string, if_not_exists: true
+    add_column :letter_queues, :template, :string, if_not_exists: true
+    add_column :letter_queues, :postage_type, :string, if_not_exists: true
+    add_column :letter_queues, :usps_payment_account_id, :bigint, if_not_exists: true
+    add_column :letter_queues, :include_qr_code, :boolean, default: true, if_not_exists: true
+    add_column :letter_queues, :letter_mailing_date, :date, if_not_exists: true
 
-    add_index :letter_queues, :type
-    add_foreign_key :letter_queues, :usps_payment_accounts, column: :usps_payment_account_id
+    add_index :letter_queues, :type, if_not_exists: true
+    add_foreign_key :letter_queues, :usps_payment_accounts, column: :usps_payment_account_id, if_not_exists: true
   end
 end
