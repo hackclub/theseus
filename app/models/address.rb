5693e053048a1ba2d529f3c62a6260b6b2789a1e
24c02 <163450896+24c02@users.noreply.github.com>
2025-05-15 16:27:22 -0400

    strip address gremlins

--- app/models/address.rb
+++ app/models/address.rb
@@ -29,7 +29,18 @@ class Address < ApplicationRecord
   include CountryEnumable
   has_country_enum
 
+  GREMLINS = [
+    "\u200E", # LEFT-TO-RIGHT MARK
+    "\u200B", # ZERO WIDTH SPACE
+  ].join
+
+  def self.strip_gremlins(str)
+    str&.delete(GREMLINS)&.presence
+  end
+
   validates_presence_of :first_name, :line_1, :city, :state, :postal_code, :country
+
+  before_validation :strip_gremlins_from_fields
 
   def name_line
     [first_name, last_name].join(" ")
@@ -59,5 +70,17 @@ class Address < ApplicationRecord
       country: country,
       origin: origin,
     ).to_s
+  end
+
+  private
+
+  def strip_gremlins_from_fields
+    self.first_name = Address.strip_gremlins(first_name)
+    self.last_name = Address.strip_gremlins(last_name)
+    self.line_1 = Address.strip_gremlins(line_1)
+    self.line_2 = Address.strip_gremlins(line_2)
+    self.city = Address.strip_gremlins(city)
+    self.state = Address.strip_gremlins(state)
+    self.postal_code = Address.strip_gremlins(postal_code)
   end
 end
