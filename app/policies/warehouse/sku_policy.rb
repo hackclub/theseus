4f3b3cfff5521f4f729bb1b2a49d82513c166dae
Parth <215315028+nystar1@users.noreply.github.com>
2026-04-15 04:53:36 +0530

    warehouse sku data api route route

--- app/policies/warehouse/sku_policy.rb
+++ app/policies/warehouse/sku_policy.rb
@@ -2,22 +2,34 @@ class Warehouse::SKUPolicy < ApplicationPolicy
   def index?
     user_can_warehouse
   end
+
   def show?
     user_can_warehouse
   end
+
   def create?
     user_is_admin
   end
+
   def new?
     user_is_admin
   end
+
   def update?
     user_is_admin
   end
+
   class Scope < ApplicationPolicy::Scope
-    # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      return scope.all if user_can_warehouse
+
+      scope.none
+    end
+
+    private
+
+    def user_can_warehouse
+      user&.can_warehouse? || user&.admin?
+    end
   end
 end
