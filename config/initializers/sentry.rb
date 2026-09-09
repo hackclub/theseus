6812843d378e589e68c5aff9804372fa136e272b
24c02 <163450896+24c02@users.noreply.github.com>
2026-09-09 12:17:48 -0400

    add Sentry before_send parameter scrubber
    applies Rails filter_parameters to request data and extras so PII
    from USPS payloads and addresses doesn't land in Sentry unscrubbed.

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,4 +5,11 @@ Sentry.init do |config|
   config.send_default_pii = false
 
   config.traces_sample_rate = 0.1
+
+  config.before_send = lambda do |event, _hint|
+    filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
+    event.request&.data = filter.filter(event.request.data) if event.request&.data.is_a?(Hash)
+    event.extra = filter.filter(event.extra) if event.extra.is_a?(Hash)
+    event
+  end
 end
